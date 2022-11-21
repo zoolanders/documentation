db889e27ee7fbd486009b6785c06b3198bc1b49f
Miljan <noreply.mn@aleksic.es>
2022-11-21 09:06:48 +0800

    cleanup

--- src/pages/_nav.js
+++ src/pages/_nav.js
@@ -3,50 +3,45 @@ import navForms from './essentials-for-yoothemepro/forms/nav.json'
 import navAccess from './essentials-for-yoothemepro/access/nav.json'
 
 const navs = {
-  '/essentials-for-yoothemepro': navYtp,
-  '/essentials-for-yoothemepro/access': navAccess,
-  '/essentials-for-yoothemepro/forms': navForms,
+    '/essentials-for-yoothemepro': navYtp,
+    '/essentials-for-yoothemepro/access': navAccess,
+    '/essentials-for-yoothemepro/forms': navForms,
 }
 
 export function getNav(forPath) {
-  const path = findNavMatch(forPath)
-  const nav = (navs[path])?.sections || []
+    const path = findNavMatch(forPath)
+    const nav = navs[path]?.sections || []
 
-  for (const section of nav) {
-
-    for (const link of section.links) {
-      link.href = link.href.replace('./', `${path}/`).replace(/\/$/, '')
+    for (const section of nav) {
+        for (const link of section.links) {
+            link.href = link.href.replace('./', `${path}/`).replace(/\/$/, '')
+        }
     }
 
-  }
-
-  return nav
+    return nav
 }
 
 export function getSubnav(forPath) {
-  const paths = Object.keys(navs).reverse()
-  const subnavs = {}
+    const paths = Object.keys(navs).reverse()
+    const subnavs = {}
 
-  console.log(forPath);
-
-  for (const path of paths) {
-    console.log(path);
-    if (path.startsWith(forPath) && (forPath !== path)) {
-      subnavs[path] = navs[path]
+    for (const path of paths) {
+        if (path.startsWith(forPath) && forPath !== path) {
+            subnavs[path] = navs[path]
+        }
     }
-  }
 
-  return subnavs
+    return subnavs
 }
 
 function findNavMatch(forPath) {
-  const paths = Object.keys(navs).reverse()
+    const paths = Object.keys(navs).reverse()
 
-  for (const path of paths) {
-    if (forPath.startsWith(path)) {
-      return path
+    for (const path of paths) {
+        if (forPath.startsWith(path)) {
+            return path
+        }
     }
-  }
 
-  return ''
+    return ''
 }
