15d604cbba7b1d494c57f996accf3c5f99c57cff
Miljan <noreply.mn@aleksic.es>
2025-01-21 11:33:00 +0800

    update icon collections

--- src/essentials-for-yootheme-pro/addons/icons/collections.data.js
+++ src/essentials-for-yootheme-pro/addons/icons/collections.data.js
@@ -3,9 +3,11 @@ import fs from 'node:fs';
 export default {
     watch: ['./_collections/*/*.json'],
     load(watchedFiles) {
-        return watchedFiles.map((file) => ({
-            ...JSON.parse(fs.readFileSync(file, 'utf-8')),
-            icon: fs.readFileSync(file.replace('collection.json', 'icon.svg'), 'utf-8'),
-        }));
+        return watchedFiles.map((file) => {
+            return {
+                ...JSON.parse(fs.readFileSync(file, 'utf-8')),
+                icon: fs.readFileSync(file.replace('.json', '.svg'), 'utf-8'),
+            };
+        });
     },
 };
