61f26cc003cd86e1d8b6597cdcf6ce3cfbfa4603
Miljan <noreply.mn@aleksic.es>
2025-02-13 17:06:28 +0800

    fix

--- .vitepress/sidebar.js
+++ .vitepress/sidebar.js
@@ -1,9 +1,7 @@
 import ZooEssentialsSidebar from '../src/essentials-for-zoo/sidebar.json';
 import YOOthemeProEssentialsSidebar from '../src/essentials-for-yootheme-pro/sidebar.json';
-import YOOthemeProEssentialsV22Sidebar from '../src/essentials-for-yootheme-pro/v2.2/sidebar.json';
 
 export default {
     '/essentials-for-zoo/': ZooEssentialsSidebar,
     '/essentials-for-yootheme-pro/': YOOthemeProEssentialsSidebar,
-    '/essentials-for-yootheme-pro/v2.2/': YOOthemeProEssentialsV22Sidebar,
 };
